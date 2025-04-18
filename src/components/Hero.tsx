998f1b02a135d40311bdc2d1ad69c81b86f39eb2
Bugzseater <pasindumadushan.org@gmail.com>
2025-04-19 03:15:05 +0530

    new

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,7 @@ function Hero() {
       gsap.fromTo(
         imageRef.current,
         { opacity: 0, scale: 0.5 },
-        { opacity: 1, scale:1, duration: 1, ease: 'power3.out', delay: 0.7 }
+        { opacity: 1, scale: 1, duration: 1, ease: 'power3.out', delay: 0.7 }
       );
     }, containerRef);
 
@@ -33,23 +33,23 @@ function Hero() {
 
   return (
     <div ref={containerRef} className='w-full p-2 mt-6 h-[90vh] flex flex-col md:flex-row gap-8'>
-        <div ref={textRef} className='w-full md:w-[50%] p-2 flex flex-col items-center text-center md:text-left'>
-            <span className='text-[60px] md:text-[150px] font-bold text-white/30'>EVOLVE</span><br></br>
-            <span className='font-bold text-white/50 text-sm md:text-base'>
-              Organized by Undergraduates of BSc. in Physics & Electronics degree program
-            </span> <br></br>
-            <span className='text-xs md:text-sm'>
-              collaborate with Electronics Students Society
-            </span>
-            <div ref={buttonsRef} className='flex md:flex-row gap-5 mt-4'>
-                <button className='bg-[#031753] p-2 rounded-md text-white'>About Event</button>
-                <button className='border border-[#1b3c9c] p-2 rounded-md text-white'>Gallery</button>
-            </div>
+      <div ref={textRef} className='w-full md:w-[50%] p-2 flex flex-col items-center text-center md:text-left'>
+        <span className='text-[60px] md:text-[150px] font-bold text-white/30'>EVOLVE</span><br />
+        <span className='font-bold text-white/50 text-sm md:text-base'>
+          Organized by Undergraduates of BSc. in Physics &amp; Electronics degree program
+        </span> <br />
+        <span className='text-xs md:text-sm'>
+          in collaboration with Electronics Students Society
+        </span>
+        <div ref={buttonsRef} className='flex flex-col md:flex-row gap-5 mt-4'>
+          <button className='bg-[#031753] p-2 rounded-md text-white'>About Event</button>
+          <button className='border border-[#1b3c9c] p-2 rounded-md text-white'>Gallery</button>
         </div>
+      </div>
 
-        <div ref={imageRef} className='w-full md:w-[50%] flex justify-center'>
-            <Image src="/roboimg.png" alt="Logo" width={300} height={300} className='md:w-auto md:h-auto'/>
-        </div>
+      <div ref={imageRef} className='w-full md:w-[50%] flex justify-center'>
+        <Image src="/roboimg.png" alt="Logo" width={300} height={300} className='md:w-auto md:h-auto' />
+      </div>
     </div>
   )
 }
